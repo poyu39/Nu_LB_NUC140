a473e36fe5cec359b256e55a955998c803eb4e3c
poyu39 <bruce920309@gmail.com>
2023-11-20 17:31:57 +0800

    feat: 新增 NewScanKey.h & NewSevenSegment.h

---
+++ Library/Nu-LB-NUC140/Include/NewScankey.h
@@ -0,0 +1,9 @@
+#ifndef SCANKEY_H
+#define SCANKEY_H
+
+void init_keypad_INT(void);
+void scan_keypad(void);
+// void close_keypad(void);
+// uint8_t ScanKey(void);
+
+#endif
---
+++ Library/Nu-LB-NUC140/Include/NewSevenSegment.h
@@ -0,0 +1,7 @@
+#ifndef __Seven_Segment_H__
+#define __Seven_Segment_H__
+
+extern void init_seven_segment(void);
+extern void show_seven_segment(uint8_t no, uint8_t number);
+extern void close_seven_segment(void);
+#endif
