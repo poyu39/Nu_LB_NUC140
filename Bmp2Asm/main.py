4fe21b1e6cd7fdb557a05f7653cf50563353dcc1
poyu39 <bruce920309@gmail.com>
2023-11-26 17:36:34 +0800

    feat: 新增圖片縮放轉換 python 腳本

--- Bmp2Asm/main.py
+++ Bmp2Asm/main.py
@@ -1,55 +1,36 @@
 import sys
-import time
-
 from PIL import Image
 
-'''
-0 = 0xC0,0x20,0x10,0x10,0x20,0xC0,0x00,0x00,0x3F,0x40,0x80,0x80,0x40,0x3F,0x00,0x00
-
-1       0 0 0 0 0 0 0 0
-2       0 0 0 0 0 0 0 0
-4       0 0 0 0 0 0 0 0
-8       0 0 0 0 0 0 0 0
-1       0 0 1 1 0 0 0 0
-2       0 1 0 0 1 0 0 0
-4       1 0 0 0 0 1 0 0
-8       1 0 0 0 0 1 0 0
------------------------
-1       1 0 0 0 0 1 0 0
-2       1 0 0 0 0 1 0 0
-4       1 0 0 0 0 1 0 0
-8       1 0 0 0 0 1 0 0
-1       1 0 0 0 0 1 0 0
-2       1 0 0 0 0 1 0 0
-4       0 1 0 0 1 0 0 0
-8       0 0 1 1 0 0 0 0
-'''
-
-if __name__ == "__main__":
-    # 輸入檔案名稱, 寬, 高
-    if len(sys.argv) == 4:
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
         filename = sys.argv[1]
         width = int(sys.argv[2])
         height = int(sys.argv[3])
+        hexorbin = str(sys.argv[4])
     else:
-        print("請輸入檔案名稱和長寬")
+        print('請輸入檔案名稱, 長, 寬, 輸出格式(hex/bin)，(若長寬非圖片長寬，會自動縮放)')
         sys.exit()
+
     bmp_img = Image.open(filename)
+    bmp_img = bmp_img.resize((width, height))
+    bmp_img = bmp_img.convert('L')
+    bmp_img = bmp_img.point(lambda x: 1 if x < 128 else 0, '1')
     bmp_bin = list(bmp_img.getdata())
-    # 15 轉成 0，其他轉成 1
-    bmp_bin = [0 if x == 15 else 1 for x in bmp_bin]
 
     # 壓縮成 16 進制
+    # 把 8 個 0 1 轉成 1 個 16 進制
     bmp_hex = []
-    for i in range(0, width * int(height / 8)):
-        temp_bin = ''
-        temp_hex = 0
-        for j in range(0, 8):
-            temp_bin += str(bmp_bin[i + j  * width])
-        # 順序反轉
-        temp_bin = temp_bin[::-1]
-        temp_hex = f'{hex(int(str(temp_bin), 2))}'
-        bmp_hex.append(temp_hex)
+    for k in range(0, int(height / 8)):
+        for i in range(k * width * 8, k * width * 8 + width):
+            temp_bin = ''
+            temp_hex = 0
+            for j in range(0, 8):
+                pixel_index = i + j * width
+                temp_bin += str(bmp_bin[pixel_index])
+            # 順序反轉
+            temp_bin = temp_bin[::-1]
+            temp_hex = hex(int(temp_bin, 2))
+            bmp_hex.append(temp_hex)
 
     # 標準化格式
     for i in range(len(bmp_hex)):
@@ -61,22 +42,21 @@ if __name__ == "__main__":
         # 補 0
         if len(bmp_hex[i]) == 3:
             bmp_hex[i] = bmp_hex[i][0:2] + '0' + bmp_hex[i][2]
-        print(bmp_hex[i])
-        # time.sleep(1)
 
     # 輸出成檔案
-    with open(filename.split('.')[0] + '.c', 'w') as f:
-        f.write('const unsigned char ' + filename.split('.')[0] + '_bin[] = {')
-        for i in range(len(bmp_bin)):
-            if i % width == 0:
-                f.write("\n\t")
-            f.write(f'{bmp_bin[i]}, ')
-        f.write('\n};')
-        f.write('\n\n')
-        
-        f.write('const unsigned char ' + filename.split('.')[0] + '_hex[] = {')
-        for i in range(len(bmp_hex)):
-            if i % width == 0:
-                f.write("\n\t")
-            f.write(f'{bmp_hex[i]}, ')
-        f.write('\n};')
+    filename = filename.split('\\')[-1].replace('.bmp', '_bmp')
+    with open(filename + '.c', 'w') as f:
+        if hexorbin == 'hex':
+            f.write('uint8_t ' + filename + '_hex[] = {')
+            for i in range(len(bmp_hex)):
+                if i % width == 0:
+                    f.write("\n\t")
+                f.write(f'{bmp_hex[i]},')
+            f.write('\n};')
+        if hexorbin == 'bin':
+            f.write('uint8_t ' + filename + '_bin[] = {')
+            for i in range(len(bmp_bin)):
+                if i % width == 0:
+                    f.write("\n\t")
+                f.write(f'{bmp_bin[i]},')
+            f.write('\n};')
