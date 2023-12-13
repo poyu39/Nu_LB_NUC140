bf40f43c54ec8254f19e0b74015705fb708e63ed
poyu39 <bruce920309@gmail.com>
2023-12-13 21:43:48 +0800

    fix: 修正卡牆

--- LAB_9_2/main.c
+++ LAB_9_2/main.c
@@ -13,7 +13,7 @@
 int32_t main() {
     int16_t ax, ay, az;
     float pitch, roll;
-    int8_t x = 8, y = 16, r = 3, dx = 0, dy = 0;   // 球的座標, 半徑, 方向
+    int8_t x = 8, y = 16, r = 4, dx = 0, dy = 0;   // 球的座標, 半徑, 方向
     int8_t vsp = 3;                                // 水平速度
     int8_t hsp = 3;                                // 垂直速度
 
@@ -32,13 +32,15 @@ int32_t main() {
         roll = atan(ay / sqrt(pow(ax, 2) + pow(az, 2))) * 180 / PI;
         
         dx = 0;
-        if (pitch > 10 || pitch < -10) dx = pitch / abs(pitch);
+        if (pitch > 10 || pitch < -10) dx = pitch / abs(pitch) * -1;
 
         dy = 0;
         if (roll > 10 || roll < -10) dy = roll / abs(roll);
 
-        if (x - r > 0 && x + r < LCD_Xmax) x += dx * vsp;
-        if (y - r > 0 && y + r < LCD_Ymax) y += dy * hsp;
+        x += dx * vsp;
+		y += dy * hsp;
+		if(x - r < 0 || x + r > LCD_Xmax) x -= dx * vsp;
+		if(y - r < 0 || y + r > LCD_Ymax) y -= dy * hsp;
 
         draw_circle_in_buffer(x, y, r, FG_COLOR, TRUE);
         show_lcd_buffer();
